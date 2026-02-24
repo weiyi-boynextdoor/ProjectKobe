8deba93a8f8c3d715c487d8bcb07e032b47506f9
weiyi <weiyiboynextdoor@163.com>
2026-02-24 17:35:32 +0800

    test audio play

--- minimax_voice_test.py
+++ minimax_voice_test.py
@@ -3,15 +3,17 @@
 import requests
 import os
 from dotenv import load_dotenv
+import pygame
 
 load_dotenv()
+pygame.mixer.init()
 
 api_key = os.getenv("MINIMAX_API_KEY")
 url = "https://api.minimax.io/v1/t2a_v2"
 
 payload = {
-    "text": "Man! What can I say? Mamba out!",
-    "model": "speech-2.8-hd",
+    "text": "Man! I can't believe how fast 20 years went by. This is crazy, this is absolutely crazy. What can I say? Mamba out!",
+    "model": "speech-2.8-turbo",
     "voice_setting": {
         "voice_id": "moss_audio_0251081c-f530-11f0-8583-3ae0c9a1b09a"
     },
@@ -31,7 +33,13 @@ headers = {
 response = requests.post(url, headers=headers, json=payload)
 
 if response.status_code == 200:
-    with open("audio_output/output.wav", "wb") as f:
+    filename = "audio_output/output.wav"
+    with open(filename, "wb") as f:
         audio_bytes = bytes.fromhex(response.json()['data']['audio'])
         f.write(audio_bytes)
-    print("Audio saved as output.wav")
+    print(f"Audio saved as {filename}")
+    # test audio play
+    sound = pygame.mixer.Sound(filename)
+    sound.play()
+    while pygame.mixer.get_busy():
+        pygame.time.Clock().tick(10)
