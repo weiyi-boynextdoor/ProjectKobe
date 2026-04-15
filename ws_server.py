6dc54b2aa270e6665eece4af1b022e0579304efe
weiyi <weiyiboynextdoor@163.com>
2026-04-15 21:44:51 +0800

    log to console

--- ws_server.py
+++ ws_server.py
@@ -26,10 +26,16 @@ os.makedirs(LOG_DIR, exist_ok=True)
 logger = logging.getLogger("ws_server")
 if not logger.handlers:
     logger.setLevel(logging.INFO)
-    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
     formatter = logging.Formatter("%(asctime)s %(levelname)s [%(name)s] %(message)s")
+
+    file_handler = logging.FileHandler(LOG_FILE, encoding="utf-8")
     file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
     logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
     logger.propagate = False
 
 class SessionManager:
